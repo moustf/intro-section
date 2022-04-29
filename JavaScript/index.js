4a06f3700273265896c47cc7bb538750bc939e02
moustf <tepro.18@gmail.com>
2022-04-29 18:06:40 +0300

    Targeting the needed elements close #6

---
+++ JavaScript/index.js
@@ -0,0 +1,16 @@
+// ? Targeting the needed elements.
+const featuresBtn = document.querySelector(".features-btn");
+const companyBtn = document.querySelector(".company-btn");
+const featuresMenu = document.querySelector(".features-menu");
+const companyMenu = document.querySelector(".company-menu");
+const arrowDownOne = document.getElementsByClassName("arrow-down")[0];
+const arrowDownTwo = document.getElementsByClassName("arrow-down")[1];
+const arrowUpOne = document.getElementsByClassName("arrow-up")[0];
+const arrowUpTwo = document.getElementsByClassName("arrow-up")[1];
+const burgerBtn = document.querySelector(".burger-menu");
+const sections = document.querySelector(".sections");
+const closeMenu = document.querySelector(".close-menu");
+const wrapperDiv = document.querySelector(".wrapper");
+
+
+
