034c2f99047e1c19ad68b16ad01908d54ac1c372
moustf <tepro.18@gmail.com>
2022-04-29 18:08:36 +0300

    Adding the event listeners to the elements when click occurs close #6

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -12,5 +12,42 @@ const sections = document.querySelector(".sections");
 const closeMenu = document.querySelector(".close-menu");
 const wrapperDiv = document.querySelector(".wrapper");
 
+// ? Adding the event listeners to the elements when click occurs.
 
+arrowDownOne.addEventListener("click", (e) => {
+  featuresMenu.style.display = "flex";
+  arrowDownOne.style.display = "none";
+  arrowUpOne.style.display = "block";
+});
 
+arrowDownTwo.addEventListener("click", (e) => {
+  companyMenu.style.display = "flex";
+  arrowDownTwo.style.display = "none";
+  arrowUpTwo.style.display = "block";
+});
+
+arrowUpOne.addEventListener("click", (e) => {
+  featuresMenu.style.display = "none";
+  arrowUpOne.style.display = "none";
+  arrowDownOne.style.display = "block";
+});
+
+arrowUpTwo.addEventListener("click", (e) => {
+  companyMenu.style.display = "none";
+  arrowUpTwo.style.display = "none";
+  arrowDownTwo.style.display = "block";
+});
+
+burgerBtn.onclick = function () {
+  sections.style.display = "grid";
+  burgerBtn.style.display = "none";
+  closeMenu.style.display = "block";
+  wrapperDiv.style.display = "block";
+};
+
+closeMenu.onclick = () => {
+  closeMenu.style.display = "none";
+  sections.style.display = "none";
+  burgerBtn.style.display = "block";
+  wrapperDiv.style.display = "none";
+};
